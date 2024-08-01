879403309b5c36bd5a6631cda75625f10557e3b6
Oleksandr Kryvonos <krausweb291985@gmail.com>
2024-08-01 18:12:02 +0300

    NextJS Dashboard Demo App - add Server fetchData logic for the Create, Add, Delete to the Invoices page - add UI for the Create and Edit pages of Invoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -32,7 +32,9 @@ export default async function Page({
 			<Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
 				<Table query={query} currentPage={currentPage} />
 			</Suspense>
-			<div className="mt-5 flex w-full justify-center"><Pagination totalPages={totalPages} /></div>
+			<div className="mt-5 flex w-full justify-center">
+				<Pagination totalPages={totalPages} />
+			</div>
 		</div>
 	);
 }
